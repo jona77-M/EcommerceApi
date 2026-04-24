21cea39a08f95c5af6837c2557de97f912707619
Rocky Malobago <rockymalobago080@gmail.com>
2026-04-24 21:35:27 +0800

    Task 7 : Fix CORS error

--- src/main/java/com/ws101/EulinMalobago/EcommerceApi/controller/ProductController.java
+++ src/main/java/com/ws101/EulinMalobago/EcommerceApi/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,7 +32,7 @@ import com.ws101.EulinMalobago.EcommerceApi.service.ProductService;
  *         operations, and returns appropriate HTTP responses for each endpoint.
  */
 @RestController
-@CrossOrigin(origins = "*")
+// @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
