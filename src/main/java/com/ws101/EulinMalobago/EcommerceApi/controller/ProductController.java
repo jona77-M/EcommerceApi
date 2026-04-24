a1491309a6ac2ab3ba5f28d6b70b682ee3de27dc
jona77-M <jonamaebeulin@gmail.com>
2026-04-24 14:34:56 +0800

    Re-update Task 5: Fetch API Fundamentals

--- src/main/java/com/ws101/EulinMalobago/EcommerceApi/controller/ProductController.java
+++ src/main/java/com/ws101/EulinMalobago/EcommerceApi/controller/ProductController.java
@@ -3,16 +3,15 @@ package com.ws101.EulinMalobago.EcommerceApi.controller;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
-import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,10 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ws101.EulinMalobago.EcommerceApi.model.Product;
 import com.ws101.EulinMalobago.EcommerceApi.service.ProductService;
 
+import jakarta.validation.Valid;
+
 /**
- * REST controller that exposes product catalog API endpoints. eulin and
+ * REST controller that exposes product catalog API endpoints. 
  * 
- * @author malobago are the authors of this code. This controller provides
+ * @author eulin and malobago are the authors of this code. This controller provides
  *         endpoints for
  *         CRUD operations on products, as well as filtering products by various
  *         criteria. It uses a ProductService to perform business logic and data
